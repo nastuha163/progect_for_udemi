5c5b21cce24df5476ba02680ecfe3e4cb924311c
nastuha163 <logistika94@mail.ru>
2021-02-02 22:43:15 +0400

    my-first-commit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('В чем сила?');
+//console.log(result);
+
+//const answer = +prompt ('В чем сила, брат?', '');
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0]=prompt('Как ваше имя?','');
+//answers[1]=prompt('Как ваша фамилия?',''); 
+//answers[2]=prompt('Сколько вам лет?',''); 
+//document.write(answers);
+
+const user ='Nastuha';
+alert(`Hello, ${user}`);
