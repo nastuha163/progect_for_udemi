d0aeb83e2be9365237ba3a96f7cb6378642a0e11
Nastya <logistika94@mail.ru>
2021-02-08 21:06:00 +0400

    leason3

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,60 @@
 "use strict";
 
-let number0fFilms;
-number0fFilms =prompt('Сколько фильмов вы уже посмотрели?');
-console.log(number0fFilms);
 
+//Цикл без повторения вопросов с условиями черезе do 
+// let a = '',
+//          b = '';
+// do {
+//          a = prompt('Один из последних просмотренных фильмов?',''),
+//          b = prompt('На сколько оцените его?','');
+// } while (a == null && b == null && a == '' && b == '' && a.length < 50);
 
-
-
-//const personalMovieDV = {
-    count:
-    movies:
-    actors:
-    genres:
-    ptivat:
-//};
-//const answer =prompt('Один из последних просмотренных фильмов?');
-//const answer =prompt('На сколько оцените его?');
-//movies: {
-//    'logan'= '8.1'
+//Задать первый вопрос через while с ограничениями в 10 симфолов и без пустого ответа
+// let isCorrectNumberOfFilms = true;
+// while(isCorrectNumberOfFilms) {
+//     if (
+//         number0fFilms.length > 10 ||
+//         number0fFilms.length === 0
+//         ) {
+//         number0fFilms = prompt('Сколько фильмов вы уже посмотрели?',''); 
+//         console.log(number0fFilms);
+//     } else {
+//         // Остановили цикл
+//         isCorrectNumberOfFilms = false;
+//     }
 //}
+
+
+//Цикл из повторения 2х вопросов с условиями черезе while
+// let iCounterLastFilms = 2;
+// let iCounterRatingFilms = 2;
+// while((iCounterLastFilms > 0) && (iCounterRatingFilms > 0)) {
+//     if (iCounterLastFilms > 0) {
+//         let lastFilms = prompt('Один из последних просмотренных фильмов?','');
+//         if (
+//             lastFilms.length > 10 ||
+//             lastFilms.length === 0 ||
+//             lastFilms === null
+//             ) {
+//             lastFilms = prompt('Один из последних просмотренных фильмов?','');
+//             console.log(lastFilms);
+//         } else {
+//             // Остановили цикл
+//             iCounterLastFilms -= 1;
+//         }
+//     }
+//     if (iCounterRatingFilms > 0) {
+//         let ratingFilms = prompt('На сколько оцените его?','');
+//         if (
+//             ratingFilms.length > 10 ||
+//             ratingFilms.length === 0 ||
+//             ratingFilms === null
+//             ) {
+//             ratingFilms = prompt('На сколько оцените его?','');
+//             console.log(ratingFilms);
+//         } else {
+//             // Остановили цикл
+//             iCounterRatingFilms -= 1;
+//         }
+//     }
+// }
--- JS/script1.js
+++ JS/script1.js
@@ -1,17 +1,71 @@
 "use strict";
-const number0fFilms =+prompt('Сколько фильмов вы уже посмотрели?','');
-let answer1 =console.log(number0fFilms);
+let number0fFilms;
+
+function start() {
+    number0fFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (number0fFilms == null || number0fFilms == '' || isNaN(number0fFilms)) {
+        number0fFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+function detectPersonalLevel() {
+    if (number0fFilms <= 10 && number0fFilms > 0) {
+        alert('Просмотренно давольно мало фильмов');
+    }
+    else if (number0fFilms <= 30 && number0fFilms > 10) {
+        alert('Вы классический зритель');
+    }
+    else if (number0fFilms > 30) {
+        alert('Вы киноман');
+    }
+    else {
+        alert('Произошла ошибка');
+    }
+}
+detectPersonalLevel();
+
 const personalMovieDV = {
     count: number0fFilms,
-    movies:{    },
-    actors:{    },
-    genres:[    ],
+    movies: {},
+    actors: {},
+    genres: [],
     ptivat: false
 };
-const a =prompt('Один из последних просмотренных фильмов?'),
-      b =prompt('На сколько оцените его?'),
-      c =prompt('Один из последних просмотренных фильмов?'),
-      d =prompt('На сколько оцените его?');
-personalMovieDV.movies[a]=b;
-personalMovieDV.movies[c]=d;
+// //Цикл из повторения 2х вопросов с условиями черезе for
+
+// function remembrMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDV.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             // Остановили цикл
+//             i--;
+//         }
+
+
+//     }
+// }
+// remembrMyFilms();
 console.log(personalMovieDV);
+
+
+// function showMyDV(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDV);
+//     }
+// }
+// showMyDV(personalMovieDV.ptivat);
+
+function writeYourGenres() {
+    for (let i = 1; i<4; i++ ) {
+        personalMovieDV.genres[i-1] = prompt('Ваш любимый жанр под номером ' + i);
+    }
+}
+writeYourGenres();
